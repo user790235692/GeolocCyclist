51b8de4054437105b73e22548dc7883846dcf3e4
Lucas <43614096+Twistere@users.noreply.github.com>
2021-10-20 21:13:40 +0200

    debug

--- src/test/java/com/mycompany/geoloccyclist/SampleTest.java
+++ src/test/java/com/mycompany/geoloccyclist/SampleTest.java
@@ -5,13 +5,10 @@ package com.mycompany.geoloccyclist;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import java.net.URL;
+import com.geoloc.selenium.Helper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -20,42 +17,37 @@ import org.testng.annotations.Test;
  *
  * @author Lucas
  */
-public class SampleTest {
+public class SampleTest extends Helper {
 
-    WebDriver driver;
-    public String  RemoteUrl = "http://localhost:4444/wd/hub";
+    WebDriver webDriver = GetDriver();
 
     @BeforeTest
     public void Setup() {
-        
-        driver = new ChromeDriver();
-        ChromeOptions handlingSSL = new ChromeOptions();
-        handlingSSL.setAcceptInsecureCerts(true);
-        driver = new ChromeDriver(handlingSSL);
-        //driver.set(new RemoteWebDriver(new URL(RemoteUrl), handlingSSL));
+        webDriver.get("https://localhost:8181/sample/");
+        webDriver.manage().window().setSize(new Dimension(776, 464));
+
     }
 
     @Test
     public void InsertUser() {
-        driver.get("https://localhost:8181/sample/");
-        driver.manage().window().setSize(new Dimension(776, 464));
-        driver.findElement(By.id("j_idt5:j_idt9")).click();
-        driver.findElement(By.id("j_idt5:j_idt9")).sendKeys("Lucas");
-        driver.findElement(By.id("j_idt5:j_idt13")).sendKeys("legeek568@gmail.com");
-        driver.findElement(By.id("j_idt5:j_idt11")).click();
-        driver.findElement(By.id("j_idt5:j_idt11")).sendKeys("Lucas Antunes");
-        driver.findElement(By.id("j_idt5:j_idt15")).click();
-        driver.findElement(By.id("j_idt5:j_idt15")).sendKeys("sdsdsd");
-        driver.findElement(By.id("j_idt5:j_idt17")).click();
-        driver.findElement(By.id("j_idt5:j_idt17")).sendKeys("sdsds");
-        driver.findElement(By.id("j_idt5:j_idt19")).click();
-        driver.findElement(By.id("j_idt5:j_idt19")).sendKeys("sdsds");
-        driver.findElement(By.cssSelector(".ui-button-text")).click();
+
+        webDriver.findElement(By.id("j_idt5:j_idt9")).click();
+        webDriver.findElement(By.id("j_idt5:j_idt9")).sendKeys("Lucas");
+        webDriver.findElement(By.id("j_idt5:j_idt13")).sendKeys("legeek568@gmail.com");
+        webDriver.findElement(By.id("j_idt5:j_idt11")).click();
+        webDriver.findElement(By.id("j_idt5:j_idt11")).sendKeys("Lucas Antunes");
+        webDriver.findElement(By.id("j_idt5:j_idt15")).click();
+        webDriver.findElement(By.id("j_idt5:j_idt15")).sendKeys("sdsdsd");
+        webDriver.findElement(By.id("j_idt5:j_idt17")).click();
+        webDriver.findElement(By.id("j_idt5:j_idt17")).sendKeys("sdsds");
+        webDriver.findElement(By.id("j_idt5:j_idt19")).click();
+        webDriver.findElement(By.id("j_idt5:j_idt19")).sendKeys("sdsds");
+        webDriver.findElement(By.cssSelector(".ui-button-text")).click();
     }
 
     @AfterTest
     public void tearDown() {
-        driver.quit();
+        webDriver.quit();
     }
 
 }
