f3e4ef48e6023cd8af8c3e0c2d77bbc2c60a04dd
Lucas <43614096+Twistere@users.noreply.github.com>
2021-10-21 16:17:42 +0200

    fix : refactor the test

--- src/test/java/com/mycompany/geoloccyclist/AddUserTest.java
+++ src/test/java/com/mycompany/geoloccyclist/AddUserTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 /*
@@ -21,14 +23,19 @@ import org.testng.annotations.Test;
  */
 public class AddUserTest {
 
-    @Test
-    public void testApp() throws MalformedURLException, InterruptedException {
-
+    WebDriver driver;
+    @BeforeTest
+    
+    public void setUp() throws MalformedURLException{
         FirefoxOptions firefoxOptions = new FirefoxOptions();
-
         firefoxOptions.setAcceptInsecureCerts(true);
-        
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), firefoxOptions);
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), firefoxOptions);
+    }
+
+
+    @Test
+    public void testApp() throws InterruptedException {
+
         driver.get("https://192.168.56.1:8181/sample/");
         Thread.sleep(500);
         driver.findElement(By.id("j_idt5:j_idt9")).click();
@@ -43,6 +50,10 @@ public class AddUserTest {
         driver.findElement(By.id("j_idt5:j_idt19")).click();
         driver.findElement(By.id("j_idt5:j_idt19")).sendKeys("6.2468465014616354");
         driver.findElement(By.cssSelector(".ui-button-text")).click();
+    }
+
+    @AfterTest
+    public void tearDown(){
         driver.quit();
     }
 }
