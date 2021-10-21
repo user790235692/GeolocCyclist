256e03f8a329842bea78b41798e4bc9e9841e558
Lucas <43614096+Twistere@users.noreply.github.com>
2021-10-21 16:35:12 +0200

    debug

--- src/test/java/com/mycompany/geoloccyclist/AddUserTest.java
+++ src/test/java/com/mycompany/geoloccyclist/AddUserTest.java
@@ -37,18 +37,18 @@ public class AddUserTest {
     public void testApp() throws InterruptedException {
 
         driver.get("https://192.168.56.1:8181/sample/");
-        driver.findElement(By.id("firstName")).click();
-        driver.findElement(By.id("firstName")).sendKeys("Rémi");
-        driver.findElement(By.id("email")).sendKeys("chocolatrie@fraise.fr");
-        driver.findElement(By.id("lastName")).click();
-        driver.findElement(By.id("lastName")).sendKeys("Vulliez");
-        driver.findElement(By.id("password")).click();
-        driver.findElement(By.id("password")).sendKeys("FlamantRose36");
-        driver.findElement(By.id("lat")).click();
-        driver.findElement(By.id("lat")).sendKeys("46.19308371013082");
-        driver.findElement(By.id("long")).click();
-        driver.findElement(By.id("long")).sendKeys("6.2468465014616354");
-        driver.findElement(By.id("send")).click();
+        driver.findElement(By.id("j_idt5:firstName")).click();
+        driver.findElement(By.id("j_idt5:firstName")).sendKeys("Rémi");
+        driver.findElement(By.id("j_idt5:email")).sendKeys("chocolatrie@fraise.fr");
+        driver.findElement(By.id("j_idt5:lastName")).click();
+        driver.findElement(By.id("j_idt5:lastName")).sendKeys("Vulliez");
+        driver.findElement(By.id("j_idt5:password")).click();
+        driver.findElement(By.id("j_idt5:password")).sendKeys("FlamantRose36");
+        driver.findElement(By.id("j_idt5:lat")).click();
+        driver.findElement(By.id("j_idt5:lat")).sendKeys("46.19308371013082");
+        driver.findElement(By.id("j_idt5:long")).click();
+        driver.findElement(By.id("j_idt5:long")).sendKeys("6.2468465014616354");
+        driver.findElement(By.id("j_idt5:send")).click();
     }
 
     @AfterTest
