8ee5877e6f97089b307a1b73d803dd92997d9e1c
Lucas <43614096+Twistere@users.noreply.github.com>
2021-10-21 17:00:05 +0200

    debug

--- src/test/java/com/mycompany/geoloccyclist/AddUserTest.java
+++ src/test/java/com/mycompany/geoloccyclist/AddUserTest.java
@@ -29,15 +29,25 @@ public class AddUserTest {
     public void setUp() throws MalformedURLException{
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.setAcceptInsecureCerts(true);
-        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), firefoxOptions);
+        driver = new RemoteWebDriver(new URL("http://firefox:4444/wd/hub"), firefoxOptions);
     }
 
 
     @Test
     public void testApp() throws InterruptedException {
 
-        driver.get("https://google.fr");
-
+        driver.get("https://192.168.56.1:8181/sample/");
+        driver.findElement(By.id("j_idt5:firstName")).click();
+        driver.findElement(By.id("j_idt5:firstName")).sendKeys("Rémi");
+        driver.findElement(By.id("j_idt5:lastName")).sendKeys("Vulliez");
+        driver.findElement(By.id("j_idt5:email")).sendKeys("chocolatrie@fraise.fr");
+        driver.findElement(By.id("j_idt5:password")).click();
+        driver.findElement(By.id("j_idt5:password")).sendKeys("FlamantRose36");
+        driver.findElement(By.id("j_idt5:lat")).click();
+        driver.findElement(By.id("j_idt5:lat")).sendKeys("46.19308371013082");
+        driver.findElement(By.id("j_idt5:long")).click();
+        driver.findElement(By.id("j_idt5:long")).sendKeys("6.2468465014616354");
+        driver.findElement(By.cssSelector(".ui-button-text")).click();
     }
 
     @AfterTest
