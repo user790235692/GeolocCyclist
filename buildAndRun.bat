309ddf8df9dd6e3e97647b3f3132756401ccee9d
Lucas <43614096+Twistere@users.noreply.github.com>
2023-02-21 09:14:35 +0100

    final projet

--- buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t com.mycompany/GeolocCyclist .
-call docker rm -f GeolocCyclist
-call docker run -d -p 9080:9080 -p 9443:9443 --name GeolocCyclist com.mycompany/GeolocCyclist
--- buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean package && docker build -t com.mycompany/GeolocCyclist .
-docker rm -f GeolocCyclist || true && docker run -d -p 9080:9080 -p 9443:9443 --name GeolocCyclist com.mycompany/GeolocCyclist
